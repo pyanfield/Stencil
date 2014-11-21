d708425b7fefd4bd3fd4f144ea92ce59c5865193
wshan <wshan@freewheel.tv>
2014-11-21 17:05:16 +0800

    init a demo for iOS

---
+++ StencilDemo/StencilDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StencilDemo
+//
+//  Created by WeiShuai Han on 11/21/14.
+//  Copyright (c) 2014 WeiShuai Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
